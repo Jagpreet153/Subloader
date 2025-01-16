ead3e0def85b4c5a5cf6b8f6d81a610766a90cee
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2025-01-16 14:38:48 +0530

    chore: config tailwind

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -74,12 +74,12 @@ export default {
 			  "100%": { transform: "translateY(0)" },
 			},
 			fadeIn: {
-			  "0%": { opacity: 0 },
-			  "100%": { opacity: 1 },
+			  "0%": { opacity: "0" },
+			  "100%": { opacity: "1" },
 			},
 			slideDown: {
-			  "0%": { transform: "translateY(-50px)", opacity: 0 },
-			  "100%": { transform: "translateY(0)", opacity: 1 },
+			  "0%": { transform: "translateY(-50px)", opacity: "0" },
+			  "100%": { transform: "translateY(0)", opacity: "1" },
 			},
 		  },
   	}
