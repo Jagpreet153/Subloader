b6c2fd0d3599c612acbb87942b29fefa8dc67359
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2025-01-16 15:24:25 +0530

    chore: add morgen

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const cors=require('cors')
+const morgan = require('morgan');
 const app= express();
 require("dotenv").config();
 
 const PORT= process.env.PORT || 3001;
-
+app.use(morgan('dev'));
 app.use(express.json());
 
 const corsOptions = {
